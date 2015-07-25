6b45e38a0ba18bb2a5bd9e38a20ed5ac283aba41
cameronaverill <cameronaverill2018@u.northwestern.edu>
2015-07-25 19:55:54 +0200

    added views

--- vacation_log/app/models/contribution.rb
+++ vacation_log/app/models/contribution.rb
@@ -2,4 +2,6 @@ class Contribution < ActiveRecord::Base
 	belongs_to :event
 	has_attached_file :avatar, :styles => {:thumb => "100X100>"}
 
+	validates_attachment_content_type :avatar, :content_type => ["avatar/jpg", "avatar/jpeg", "avatar/png", "avatar/gif"]
+
 end
