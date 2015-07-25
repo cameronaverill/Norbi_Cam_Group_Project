15ab26359f71fcc0f305c92c5230b45874db0037
cameronaverill <cameronaverill2018@u.northwestern.edu>
2015-07-25 17:47:09 +0200

    finished events controller

--- vacation_log/app/controllers/events_controller.rb
+++ vacation_log/app/controllers/events_controller.rb
@@ -5,13 +5,19 @@ class EventsController < ApplicationController
 
 	def create
 		@event = Event.new(event_params)
+		if @event.save
+			redirect_to event_path(@event)
+		else
+			render :new
+		end
 	end
 
 	def new
-
+		@event = Event.new
 	end
 
 	def edit
+		@event = Event.find(params[:id])
 	end
 
 	def show
@@ -19,9 +25,19 @@ class EventsController < ApplicationController
 	end
 
 	def update
+		@event = Event.find(params[:id])
+		if @event.update(event_params)
+			redirect_to @event
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
+		@event = Event.find(params[:id])
+		@event.destroy
+
+		redirect_to events_path
 	end
 
 	private
