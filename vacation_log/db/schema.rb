3e47fcc15b57c2e8fa70ed0cdd3f9a44b58b3eda
cameronaverill <cameronaverill2018@u.northwestern.edu>
2015-07-25 18:31:31 +0200

    fixed gem

--- vacation_log/db/schema.rb
+++ vacation_log/db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150725153210) do
+ActiveRecord::Schema.define(version: 20150725162258) do
+
+  create_table "contributions", force: :cascade do |t|
+    t.string   "contributor"
+    t.string   "text"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
