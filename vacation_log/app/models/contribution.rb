dc7498e109d1c8b99bb8ac664b0db2b992b1668e
cameronaverill <cameronaverill2018@u.northwestern.edu>
2015-07-26 22:47:41 +0200

    reverted back to working version

--- vacation_log/app/models/contribution.rb
+++ vacation_log/app/models/contribution.rb
@@ -1,7 +1,7 @@
 class Contribution < ActiveRecord::Base
 	belongs_to :event
-	has_attached_file :avatar, :styles => {:thumb => "100X100>"}
+	has_attached_file :avatar, :styles => { :larger => '400x400', :medium => "200x200>", :thumb => "100x100>" },:default_url => '/system/users/avatars/missing.jpg'
 
-	validates_attachment_content_type :avatar, :content_type => ["avatar/jpg", "avatar/jpeg", "avatar/png", "avatar/gif"]
+	validates_attachment_content_type :avatar, :content_type => /\Aavatar/
 
 end
