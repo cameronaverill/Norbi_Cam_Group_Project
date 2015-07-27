64119ccc4ea1e59a71319eb89670876229059c5c
cameronaverill <cameronaverill2018@u.northwestern.edu>
2015-07-27 10:33:18 +0200

    for Heroku

--- vacation_log/app/controllers/contributions_controller.rb
+++ vacation_log/app/controllers/contributions_controller.rb
@@ -12,6 +12,26 @@ class ContributionsController < ApplicationController
     	redirect_to event_path(@event)
 	end
 
+	def edit
+		@event = Event.find(params[:event_id])
+		@contribution = @event.contributions.find(params[:id])
+	end
+
+	def update
+		@event = Event.find(params[:id])
+		@contribution = @event.contributions.find(params[:id])
+		if @contribution.update(contribution_params)
+			redirect_to @event
+		else
+			render 'edit'
+		end
+	end
+
+	def show
+		@event = Event.find(params[:event_id])
+		@contributions = @event.contributions.find(params[:id])
+	end
+
 	private
 	def contribution_params
 		params.require(:contribution).permit(:text, :contributor, :photo)
