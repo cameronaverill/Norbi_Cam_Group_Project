88d96b6da7027c545fb4f3e76a5401bb788ab484
cameronaverill <cameronaverill2018@u.northwestern.edu>
2015-07-27 09:37:11 +0200

    fixed double error

--- vacation_log/db/seeds.rb
+++ vacation_log/db/seeds.rb
@@ -8,5 +8,7 @@
 
 Event.delete_all
 
-cape = Event.create name: 'Cape of Good Hope', date: "July 18, 2015", rating: 4, likes: 0
-surfing = Event.create name: "Surfing at Muizenberg", date: "July 25, 2015", rating: 4, likes: 0
+cape = Event.create! name: 'Cape of Good Hope', date: "July 18, 2015", rating: 4, likes: 0
+surfing = Event.create! name: "Surfing at Muizenberg", date: "July 25, 2015", rating: 4, likes: 0
+shark = Event.create! name: 'Shark Cage Diving', date: "July 27, 2015", rating: 5
+
